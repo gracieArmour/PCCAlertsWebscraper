3b71a53fa55aa0c59084e66da6d94c07e17b2a3d
Jackie Armour <43764940+jackieArmour@users.noreply.github.com>
2020-02-10 10:28:14 -0800

    Added code to grab specific p tag text

--- PythonCode/Webscraper.py
+++ PythonCode/Webscraper.py
@@ -13,11 +13,14 @@ def scrape():
     
     page = requests.get(url)
     soup = BeautifulSoup(page.content, 'html.parser')
+    statusContainer = soup.find_all('div')[2]
+    pTag = statusContainer.find_all('p')[2]
+    statusText = pTag.get_text()
     
     if page.status_code==200:
         print("Page Accessed!")
-        #print("Contents: "+str(soup))
+        print(statusText)
     else:
         print("Error code: " + str(page.status_code) + " Page not accessible")
-    return soup
+    return statusText
 
