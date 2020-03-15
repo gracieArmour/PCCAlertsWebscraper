c961b54e93c42746d0e95f531f731af441ad5801
Jackie Armour <43764940+jackieArmour@users.noreply.github.com>
2020-03-14 23:54:32 -0700

    Updated webscraper to latest version

--- PythonCode/FinalCode/webscraper.py
+++ PythonCode/FinalCode/webscraper.py
@@ -13,9 +13,14 @@ def scrape(url):
             soup = BeautifulSoup(page.content, 'html.parser')
             tags = [str(tag) for tag in soup.find_all()]
             for i,content in enumerate(tags):
-                if (("current status" in content.lower()) and ("h3" in content.lower())):
-                    statusTag = tags[i+1]
-                    statusText = statusTag.split('>')[1].split('<')[0]
+                if ("h3" in content.lower()):
+                    while not('div' in tags[i]):
+                        statusTag = tags[i]
+                        if not('statusText' in locals()):
+                            statusText = statusTag.split('>')[1].split('<')[0]
+                        else:
+                            statusText = statusText+" "+statusTag.split('>')[1].split('<')[0]
+                        i += 1
             if not('statusText' in locals()):
                 statusText = "Status Header not found"
         else:
